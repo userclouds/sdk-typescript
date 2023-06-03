c8229dffac6c424e8ddb6fb6b299f8c186fff14f
James Wang <james@userclouds.com>
2023-06-03 06:52:55 -0700

    Updating models for mutators, transformers, accesors, and access policies

---
+++ userstore/models/index.ts
@@ -0,0 +1,26 @@
+import AccessPolicy from './access_policy';
+import Accessor, { ColumnOutputConfig } from './accessor';
+import Column from './column';
+import Mutator, { ColumnInputConfig } from './mutator';
+import Purpose from './purpose';
+import Transformer from './transformer';
+import User from './user';
+import UserSelectorConfig from './user_selector';
+import Validator from './validator';
+import ResourceID from './resource_id';
+
+export type {
+  AccessPolicy,
+  Accessor,
+  Column,
+  ColumnInputConfig,
+  ColumnOutputConfig,
+  Mutator,
+  Purpose,
+  Transformer,
+  User,
+  UserSelectorConfig,
+  Validator,
+};
+
+export { ResourceID };
